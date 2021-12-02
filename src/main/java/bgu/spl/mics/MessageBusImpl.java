65bf5112ddd682f1edc6ec330546787aa5deae86
lahmyroy <59954084+lahmyroy@users.noreply.github.com>
2021-12-02 17:10:44 +0200

    update

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,6 +1,11 @@
 package bgu.spl.mics;
 
+import java.security.Provider;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
@@ -9,25 +14,45 @@ import java.util.Map;
  */
 public class MessageBusImpl implements MessageBus {
 
-	// lock A
-	// subscribers hash map
-	// msgType -> services, lastCalled
-	// msgType -> services, lastCalled
+	// A (Subscriptions)
+	// subscribers hash map (lock (1) for adding event - to prevent 2 added msgTypes the same time)
+	// msgType (subscription) -> services, lastCalled, lock (2)
+	// msgType (subscription) -> services, lastCalled, lock (2)
 	//
 
-	// lock B
-	// services hash map
-	// service -> queue = {}
-	// service -> queue = {}
-	// service -> queue = {}
+	// read from A by msgType locked - only when updating the lastCalled (2)
+	// write to A by msgType locked (2)
 
-	// adding service will not conflict
-	// removing service should block pulling event from
-	//
+	// B (registeredServices)
+	// services hash map (lock (3) for adding service - to prevent 2 added/removed services the same time) / for safety reasons - will not be a problem
+	// service -> queue = { events }, lock (4)
+	// service -> queue = {}, lock (4)
+	// service -> queue = {}, lock (4)
+
+	// adding and removing from a queue should be interchangeably locked (4 - will be a monitor of the service itself)
+
+	// functions:
+	// getMsgType - get (services, lastCalled, lock) by the msgType
+
+	HashMap<Class<? extends Message>, Subscription> Subscriptions;
+	HashMap<MicroService, Queue<MicroService>> registeredServices;
+
+	private static MessageBusImpl singletonInstance = new MessageBusImpl();
+
+	private MessageBusImpl() {
+		Subscriptions = new HashMap<Class<? extends Message>, Subscription>();
+		registeredServices = new HashMap<MicroService, Queue<MicroService>>();
+	}
+
+	/**
+	 * Retrieves the singleton instance
+	 */
+	public static MessageBus getInstance() {
+		return singletonInstance;
+	}
 
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-
 
 
 	}
@@ -72,9 +97,32 @@ public class MessageBusImpl implements MessageBus {
 	@Override
 	public Message awaitMessage(MicroService m) throws InterruptedException {
 		// TODO Auto-generated method stub
+		while (){
+			wait();
+		}
+
+
 		return null;
 	}
 
-	
+	private class Subscription {
+		private int lastCalled = 0;
+		private ArrayList<MicroService> services = new ArrayList<MicroService>();
+		public Subscription(MicroService initService) {
+			services.add(initService);
+		}
+		public synchronized void addService(MicroService service){
+			services.add(service);
+		}
+		public synchronized MicroService getNextService() {
+			MicroService service = services.get(lastCalled);
+			// update last called according to RoundRobin Rules
+			lastCalled = (lastCalled + 1) % services.size();
+			return  service;
+		}
+		public synchronized ArrayList<MicroService> getAllServices(){
+			return services;
+		}
+	}
 
 }
--- src/main/java/bgu/spl/mics/application/objects/CPU.java
+++ src/main/java/bgu/spl/mics/application/objects/CPU.java
@@ -6,4 +6,5 @@ package bgu.spl.mics.application.objects;
  * Add fields and methods to this class as you see fit (including public methods and constructors).
  */
 public class CPU {
+
 }
